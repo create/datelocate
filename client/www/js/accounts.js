5bff38e429b7c7497a78d880ef2cb64e0957c954
Matt Robinson <mattlrobinson7@gmail.com>
2014-04-25 23:52:19 -0700

    account picture

--- client/www/js/accounts.js
+++ client/www/js/accounts.js
@@ -3,6 +3,7 @@
 function findName() {
 	FB.api('/me', function(response) {
     $('#username').html("Welcome " + response.name);
+    $('#userphoto').html(<img src=response.userphoto />)
     console.log(response.name);
 	});
 }
