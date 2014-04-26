90a205a6a328e0e6b10ee5d4ccf520a6d312402b
Matt Robinson <mattlrobinson7@gmail.com>
2014-04-26 05:58:28 -0700

    redirect

--- client/www/js/accounts.js
+++ client/www/js/accounts.js
@@ -18,6 +18,7 @@ window.fbAsyncInit = function() {
       // The response object is returned with a status field that lets the app know the current
       // login status of the person. In this case, we're handling the situation where they 
       // have logged in to the app.
+      window.top.location = "http://datefinder.herokuapp.com/#map-page";
       testAPI();
     } else if (response.status === 'not_authorized') {
       // In this case, the person is logged into Facebook, but not into the app, so we call
@@ -27,6 +28,7 @@ window.fbAsyncInit = function() {
       // (1) JavaScript created popup windows are blocked by most browsers unless they 
       // result from direct interaction from people using the app (such as a mouse click)
       // (2) it is a bad experience to be continually prompted to login upon page load.
+      console.log("notloggedin");
       FB.login();
     } else {
       // In this case, the person is not logged into Facebook, so we call the login() 
@@ -34,6 +36,7 @@ window.fbAsyncInit = function() {
       // of whether they are logged into the app. If they aren't then they'll see the Login
       // dialog right after they log in to Facebook. 
       // The same caveats as above apply to the FB.login() call here.
+      console.log("notloggedin");
       FB.login();
     }
   });
@@ -72,7 +75,6 @@ window.fbAsyncInit = function() {
     });
   }
 function findName() {
-	
 	FB.api('/me', function(response) {
 		console.log("Success" + response.name);
 		if (response.name != undefined) {
