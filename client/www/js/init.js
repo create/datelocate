7674914180c1fa28f0ff809474437b9ea0793ff6
Jeff <jeffpyke@uw.edu>
2014-10-14 01:19:30 +0200

    cleanup

--- client/www/js/init.js
+++ client/www/js/init.js
@@ -36,8 +36,8 @@
 		jQuery.fn.n33_formerize=function(){var _fakes=new Array(),_form = jQuery(this);_form.find('input[type=text],textarea').each(function() { var e = jQuery(this); if (e.val() == '' || e.val() == e.attr('placeholder')) { e.addClass('formerize-placeholder'); e.val(e.attr('placeholder')); } }).blur(function() { var e = jQuery(this); if (e.attr('name').match(/_fakeformerizefield$/)) return; if (e.val() == '') { e.addClass('formerize-placeholder'); e.val(e.attr('placeholder')); } }).focus(function() { var e = jQuery(this); if (e.attr('name').match(/_fakeformerizefield$/)) return; if (e.val() == e.attr('placeholder')) { e.removeClass('formerize-placeholder'); e.val(''); } }); _form.find('input[type=password]').each(function() { var e = jQuery(this); var x = jQuery(jQuery('<div>').append(e.clone()).remove().html().replace(/type="password"/i, 'type="text"').replace(/type=password/i, 'type=text')); if (e.attr('id') != '') x.attr('id', e.attr('id') + '_fakeformerizefield'); if (e.attr('name') != '') x.attr('name', e.attr('name') + '_fakeformerizefield'); x.addClass('formerize-placeholder').val(x.attr('placeholder')).insertAfter(e); if (e.val() == '') e.hide(); else x.hide(); e.blur(function(event) { event.preventDefault(); var e = jQuery(this); var x = e.parent().find('input[name=' + e.attr('name') + '_fakeformerizefield]'); if (e.val() == '') { e.hide(); x.show(); } }); x.focus(function(event) { event.preventDefault(); var x = jQuery(this); var e = x.parent().find('input[name=' + x.attr('name').replace('_fakeformerizefield', '') + ']'); x.hide(); e.show().focus(); }); x.keypress(function(event) { event.preventDefault(); x.val(''); }); });  _form.submit(function() { jQuery(this).find('input[type=text],input[type=password],textarea').each(function(event) { var e = jQuery(this); if (e.attr('name').match(/_fakeformerizefield$/)) e.attr('name', ''); if (e.val() == e.attr('placeholder')) { e.removeClass('formerize-placeholder'); e.val(''); } }); }).bind("reset", function(event) { event.preventDefault(); jQuery(this).find('select').val(jQuery('option:first').val()); jQuery(this).find('input,textarea').each(function() { var e = jQuery(this); var x; e.removeClass('formerize-placeholder'); switch (this.type) { case 'submit': case 'reset': break; case 'password': e.val(e.attr('defaultValue')); x = e.parent().find('input[name=' + e.attr('name') + '_fakeformerizefield]'); if (e.val() == '') { e.hide(); x.show(); } else { e.show(); x.hide(); } break; case 'checkbox': case 'radio': e.attr('checked', e.attr('defaultValue')); break; case 'text': case 'textarea': e.val(e.attr('defaultValue')); if (e.val() == '') { e.addClass('formerize-placeholder'); e.val(e.attr('placeholder')); } break; default: e.val(e.attr('defaultValue')); break; } }); window.setTimeout(function() { for (x in _fakes) _fakes[x].trigger('formerize_sync'); }, 10); }); return _form; };
 
 	// scrolly
-		jQuery.fn.n33_scrolly = function(offset) {				
-			
+		jQuery.fn.n33_scrolly = function(offset) {
+
 			jQuery(this).click(function(e) {
 				var h = jQuery(this).attr('href'), target;
 
@@ -45,7 +45,7 @@
 				{
 					var pos = Math.max(target.offset().top, 0);
 					e.preventDefault();
-					
+
 					if (offset)
 					{
 						if (typeof(offset) == 'function')
@@ -53,7 +53,7 @@
 						else
 							pos -= offset;
 					}
-					
+
 					jQuery('body,html').animate({ scrollTop: pos }, 1000, 'swing');
 				}
 			});
@@ -84,9 +84,9 @@
             center: new google.maps.LatLng(47.656080, -122.309353),
             disableDefaultUI: true,
             zoom: 13,
-            tilt: 45,  
+            tilt: 45,
         };
-		
+
 $(window).load(function() {
     $('#first header').waypoint(function(up) {
 		$('#first header .fadein').each(function(index) {
@@ -162,10 +162,10 @@ var getDates = function(LatLng, map) {
                         return function() {
                         	window.location.replace("map.html?did="+did);
                         }
-                        
+
                     };
                     google.maps.event.addListener(marker, 'click', markerClickCallback(did));
-                
+
                 }
             }
         });
