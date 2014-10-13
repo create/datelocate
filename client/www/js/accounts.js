bc1f459f8d4d9427d99a85b5c79737f54d6a0fe6
Jeff <jeffpyke@uw.edu>
2014-10-14 01:35:16 +0200

    update center, text

--- client/www/js/accounts.js
+++ client/www/js/accounts.js
@@ -10,32 +10,32 @@ window.fbAsyncInit = function () {
 
   // Here we subscribe to the auth.authResponseChange JavaScript event. This event is fired
   // for any authentication related change, such as login, logout or session refresh. This means that
-  // whenever someone who was previously logged out tries to log in again, the correct case below 
-  // will be handled. 
+  // whenever someone who was previously logged out tries to log in again, the correct case below
+  // will be handled.
   FB.Event.subscribe('auth.authResponseChange', function(response) {
-    // Here we specify what we do with the response anytime this event occurs. 
+    // Here we specify what we do with the response anytime this event occurs.
     if (response.status === 'connected') {
       // The response object is returned with a status field that lets the app know the current
-      // login status of the person. In this case, we're handling the situation where they 
+      // login status of the person. In this case, we're handling the situation where they
       // have logged in to the app.
-      
+
       testAPI();
     } else if (response.status === 'not_authorized') {
       // In this case, the person is logged into Facebook, but not into the app, so we call
-      // FB.login() to prompt them to do so. 
-      // In real-life usage, you wouldn't want to immediately prompt someone to login 
+      // FB.login() to prompt them to do so.
+      // In real-life usage, you wouldn't want to immediately prompt someone to login
       // like this, for two reasons:
-      // (1) JavaScript created popup windows are blocked by most browsers unless they 
+      // (1) JavaScript created popup windows are blocked by most browsers unless they
       // result from direct interaction from people using the app (such as a mouse click)
       // (2) it is a bad experience to be continually prompted to login upon page load.
       console.log("notloggedin");
-      
+
       FB.login();
     } else {
-      // In this case, the person is not logged into Facebook, so we call the login() 
+      // In this case, the person is not logged into Facebook, so we call the login()
       // function to prompt them to do so. Note that at this stage there is no indication
       // of whether they are logged into the app. If they aren't then they'll see the Login
-      // dialog right after they log in to Facebook. 
+      // dialog right after they log in to Facebook.
       // The same caveats as above apply to the FB.login() call here.
       console.log("notloggedin");
       FB.login();
@@ -52,8 +52,8 @@ window.fbAsyncInit = function () {
    ref.parentNode.insertBefore(js, ref);
   }(document));
 
-  // Here we run a very simple test of the Graph API after login is successful. 
-  // This testAPI() function is only called in those cases. 
+  // Here we run a very simple test of the Graph API after login is successful.
+  // This testAPI() function is only called in those cases.
   function testAPI() {
     console.log('Welcome!  Fetching your information.... ');
     FB.api('/me', function(response) {
@@ -83,13 +83,13 @@ function findName() {
 			$('#not-logged-in').css("display", "none");
 			$('#logged-in').css("display", "default");
 			$('#username').html("Welcome <span style='font-weight: 300;'>" + response.name+"</span>");
-			
+
 		} else {
 			$('#logged-in').css("display", "none");
 			$('#not-logged-in').css("display", "default");
 			$('#username').html("Not logged in");
 		}
-    
+
 	});
   getReq(baseUrl + "account", function(data, status) {
         var datesArr = data.user.voted_dates;
@@ -107,17 +107,17 @@ function findName() {
         dateSet.add(datesArr);
         var keys = dateSet.keys();
 
-        //get array of date id's 
+        //get array of date id's
         //for each one, add it to the div in a similar fashion to the reviews
         for (var i = 0; i < keys.length; i++) {
           getReq(baseUrl + "getdate/" + keys[i], function (res) {
             console.log(res);
                 if (res != null && res.date != null) {
-                  $('<li class="donedate"><a href="#" onclick="(function(){currentDID = \''+res.date._id+'\'; onDetailsLoad();})();" ><strong>' + res.date.name +
+                  $('<li class="donedate"><a href="#" onclick="(function(){currentDID = \''+res.date._id+'\'; onDetailsLoad(true);})();" ><strong>' + res.date.name +
                     '</strong> at '+res.date.location_name+'</a></li>').hide().appendTo(list).slideDown();
                 }
           });
-          
+
         }
         list.listview("refresh");
       }).fail(function(err) {
