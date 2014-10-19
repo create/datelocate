1695548bdb1c96e4f7a27183009464b49f7292a7
Jeff <jeffpyke@uw.edu>
2014-10-19 19:18:41 +0200

    Android updates

--- client/www/js/accounts.js
+++ client/www/js/accounts.js
@@ -1,60 +1,90 @@
 //accounts.js
-window.fbAsyncInit = function () {
-    FB.init({
-        appId      : '233709206819119',
-        status     : true, // check login status
-        cookie     : true, // enable cookies to allow the server to access the session
-        xfbml      : true  // parse XFBML
-    });
-
-    // Here we subscribe to the auth.authResponseChange JavaScript event. This event is fired
-    // for any authentication related change, such as login, logout or session refresh. This means that
-    // whenever someone who was previously logged out tries to log in again, the correct case below
-    // will be handled.
-    FB.Event.subscribe('auth.authResponseChange', function(response) {
-        // Here we specify what we do with the response anytime this event occurs.
+document.addEventListener('deviceready', function() {
+    console.log("deviceready");
+    if (typeof CDV === 'undefined') {
+        console.log('CDV variable does not exist. Check that you have included cdv-plugin-fb-connect.js correctly');
+    }
+    if (typeof FB === 'undefined') {
+        console.log('FB variable does not exist. Check that you have included the Facebook JS SDK file.');
+    }
+    try {
+        FB.init({
+            appId: "233709206819119",
+            nativeInterface: CDV.FB,
+            useCachedDialogs: false
+        });
+    } catch (e) {
+        console.log(e);
+    }
+    $('#fb-login-button').show();
+    $('#fb-login-button').click(function(e) {
+      e.preventDefault();
+      CDV.FB.login({ scope: "email" }, function(response) {
         if (response.status === 'connected') {
-            // The response object is returned with a status field that lets the app know the current
-            // login status of the person. In this case, we're handling the situation where they
-            // have logged in to the app.
-
-            signIn();
-        } else if (response.status === 'not_authorized') {
-            // In this case, the person is logged into Facebook, but not into the app, so we call
-            // FB.login() to prompt them to do so.
-            // In real-life usage, you wouldn't want to immediately prompt someone to login
-            // like this, for two reasons:
-            // (1) JavaScript created popup windows are blocked by most browsers unless they
-            // result from direct interaction from people using the app (such as a mouse click)
-            // (2) it is a bad experience to be continually prompted to login upon page load.
-            console.log("not_auth");
-            setLoginStatus(false);
+          alert('logged in');
+          signIn();
         } else {
-            // In this case, the person is not logged into Facebook, so we call the login()
-            // function to prompt them to do so. Note that at this stage there is no indication
-            // of whether they are logged into the app. If they aren't then they'll see the Login
-            // dialog right after they log in to Facebook.
-            // The same caveats as above apply to the FB.login() call here.
-            console.log("else notloggedin");
-                setLoginStatus(false);
+          alert('not logged in');
         }
+      });
     });
-};
+}, false);
+// window.fbAsyncInit = function () {
+//     FB.init({
+//         appId      : '233709206819119',
+//         status     : true, // check login status
+//         cookie     : true, // enable cookies to allow the server to access the session
+//         xfbml      : true  // parse XFBML
+//     });
+
+//     // Here we subscribe to the auth.authResponseChange JavaScript event. This event is fired
+//     // for any authentication related change, such as login, logout or session refresh. This means that
+//     // whenever someone who was previously logged out tries to log in again, the correct case below
+//     // will be handled.
+//     FB.Event.subscribe('auth.authResponseChange', function(response) {
+//         // Here we specify what we do with the response anytime this event occurs.
+//         if (response.status === 'connected') {
+//             // The response object is returned with a status field that lets the app know the current
+//             // login status of the person. In this case, we're handling the situation where they
+//             // have logged in to the app.
+
+//             signIn();
+//         } else if (response.status === 'not_authorized') {
+//             // In this case, the person is logged into Facebook, but not into the app, so we call
+//             // FB.login() to prompt them to do so.
+//             // In real-life usage, you wouldn't want to immediately prompt someone to login
+//             // like this, for two reasons:
+//             // (1) JavaScript created popup windows are blocked by most browsers unless they
+//             // result from direct interaction from people using the app (such as a mouse click)
+//             // (2) it is a bad experience to be continually prompted to login upon page load.
+//             console.log("not_auth");
+//             setLoginStatus(false);
+//         } else {
+//             // In this case, the person is not logged into Facebook, so we call the login()
+//             // function to prompt them to do so. Note that at this stage there is no indication
+//             // of whether they are logged into the app. If they aren't then they'll see the Login
+//             // dialog right after they log in to Facebook.
+//             // The same caveats as above apply to the FB.login() call here.
+//             console.log("else notloggedin");
+//                 setLoginStatus(false);
+//         }
+//     });
+// };
 
 // Load the SDK asynchronously
-(function(d){
-    var js, id = 'facebook-jssdk', ref = d.getElementsByTagName('script')[0];
-    if (d.getElementById(id)) {return; }
-    js = d.createElement('script'); js.id = id; js.async = true;
-    js.src = "//connect.facebook.net/en_US/all.js";
-    ref.parentNode.insertBefore(js, ref);
-}(document));
+// (function(d){
+//     var js, id = 'facebook-jssdk', ref = d.getElementsByTagName('script')[0];
+//     if (d.getElementById(id)) {return; }
+//     js = d.createElement('script'); js.id = id; js.async = true;
+//     js.src = "http://connect.facebook.net/en_US/all.js";
+//     ref.parentNode.insertBefore(js, ref);
+// }(document));
 
 // Here we run a very simple test of the Graph API after login is successful.
 // This testAPI() function is only called in those cases.
 function signIn() {
     console.log('Welcome!  Fetching your information.... ');
-    FB.api('/me', function (response) {
+    var cb = function (response) {
         console.log('Good to see you, ' + response.name + '.');
         console.log("fb id: " + response.id);
         window.localStorage.userid = response.id;
@@ -72,7 +102,14 @@ function signIn() {
                 console.log(err.responseJSON.errors);
             });
         });
-    });
+    };
+    if (typeof CDV === 'undefined') {
+      FB.api('/me', cb);
+    } else {
+      CDV.FB.api('/me', cb);
+    }
+
+
 }
 function setLoginStatus(inOrOut, name) {
     if (inOrOut) {
@@ -99,7 +136,6 @@ function getAccountInfo() {
         var list = $('#dates-list');
         var demo = new countUp("reviewcount", 0, kudoPoints, 0, 2.2, options);
         demo.start();
-        console.log(kudoPoints + " " + currentPoints + " " + $('#dates-list').children().length);
         if (kudoPoints != currentPoints || $('#dates-list').children().length == 0) {
             $('.donedate', list).remove();
             var dateSet = new MiniSet();
@@ -128,10 +164,17 @@ $(document).ready(function() {
     $('#logout').click(function (e) {
         e.stopImmediatePropagation();
         e.preventDefault();
-        FB.api('me/permissions', 'delete', function (res) {
+        if (typeof CDV !== 'undefined') {
+          CDV.FB.logout(function (res) {
+            setLoginStatus(false);
+          });
+        } else {
+          FB.api('me/permissions', 'delete', function (res) {
             console.log(res);
             setLoginStatus(false);
-        });
+          });
+        }
+
     });
     $('#accountbutton').click(function(e) {
         getAccountInfo();
